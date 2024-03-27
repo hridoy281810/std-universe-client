274df0f88a0b1199a37d99c710ff0447af9ebf4f
hridoy281810 <anisurrahman01815001904@gmail.com>
2024-03-27 14:02:26 +0600

    handle dynamic rolebase login

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -1,12 +1,23 @@
 import { ReactNode } from "react";
-import { useAppSelector } from "../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { Navigate } from "react-router-dom";
-import { selectCurrentToken } from "../../redux/features/auth/authSlice";
-
-const ProtectedRoute = ({children}:{children:ReactNode}) => {
+import { logout, selectCurrentToken, selectCurrentUser } from "../../redux/features/auth/authSlice";
+import { useSelector } from "react-redux";
+type TProtectedRoute={
+  children:ReactNode
+  role:string | undefined
+}
+const ProtectedRoute = ({children,role}:TProtectedRoute) => {
+  console.log(role);
+   const user = useSelector(selectCurrentUser)
+   console.log(user);
+   const dispatch = useAppDispatch()
     const token = useAppSelector(selectCurrentToken)
     // console.log(token);
-    
+    if(role !== undefined && role !== user?.role){
+             dispatch(logout())
+     return <Navigate to='/login' replace={true}/>
+    }
     if(!token){
         return <Navigate to='/login' replace={true}/>
     }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -8,6 +8,7 @@ import { adminPaths } from "./admin.routes";
 import { routeGenerator } from "../utils/routesGenerators";
 import { facultyPaths } from "./faculty.routes";
 import { studentPaths } from "./student.routes";
+import ProtectedRoute from "../components/layout/ProtectedRoute";
 
 const router = createBrowserRouter([
     {
@@ -26,17 +27,26 @@ const router = createBrowserRouter([
     },
     {
       path: "/admin", 
-      element:<App />,
+      element:
+      <ProtectedRoute role="admin">
+        <App />
+      </ProtectedRoute>,
       children:routeGenerator(adminPaths) ,
     },
     {
       path: "/faculty", 
-      element: <App />,
+      element:    
+      <ProtectedRoute role="faculty">
+      <App />,
+    </ProtectedRoute>,
       children:routeGenerator(facultyPaths),
     },
     {
       path: "/student", 
-      element: <App />,
+      element:   
+       <ProtectedRoute role="student">
+      <App />
+    </ProtectedRoute>,
       children:routeGenerator(studentPaths) ,
     },
     {
